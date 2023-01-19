04e5012b9fb8127d72826fb6099ca49043f05c27
Adrian Deon <sushdawg@Adrians-MacBook-Pro.local>
2023-01-19 03:26:45 -0600

    added name test

--- main/__test__/Employee.test.js
+++ main/__test__/Employee.test.js
@@ -1,7 +1,13 @@
 const Employee = require("../lib/Employee");
 
-test("Can an Employee be created", () => {
-const ne = new Employee();
-expect( ne instanceof Employee).toBe(true);
+test("If an Employee can be created", () => {
+ const ne = new Employee();
+ expect( ne instanceof Employee).toBe(true);
+});
+
+test("If the name can be created using constructor class", () => {
+ const expectedName = "Loree";
+ const ne = new Employee(expectedName);
+ expect(ne.expectedName).toBe(expectedName);
 });
 
