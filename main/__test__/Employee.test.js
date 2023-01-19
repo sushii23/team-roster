bd262f8ea2dd035ed864b617ddb35a3727c3affd
Adrian Deon <sushdawg@Adrians-MacBook-Pro.local>
2023-01-19 07:17:57 -0600

    commented

--- main/__test__/Employee.test.js
+++ main/__test__/Employee.test.js
@@ -8,41 +8,41 @@ test("If an Employee can be created", () => {
 test("If the name can be created using constructor class", () => {
  const expectedName = "Loree";
  const ne = new Employee(expectedName);
- expect(ne.name).toBe(expectedName);
+ expect(ne.name).toEqual(expectedName);
 });
 
 test("If the id can be set through constuctor", () => {
   const expectedID = 436036;
   const ne = new Employee("foo",expectedID);
-  expect(ne.id).toBe(expectedID);
+  expect(ne.id).toEqual(expectedID);
 });
 
 test("if the email can be set through constructor", () => {
   const expectedEA = "foo@foo.com";
   const ne = new Employee("foo",0,expectedEA);
-  expect(ne.email).toBe(expectedEA);
+  expect(ne.email).toEqual(expectedEA);
 });
-
+//get method sections//
 test("If you're able to get name from the method getName()", () => {
     const expectedName = "Loree";
     const ne = new Employee(expectedName);
-    expect(ne.getName()).toBe(expectedName);
+    expect(ne.getName()).toEqual(expectedName);
 });
 
 test("If you're able to get Id from the method getId()", () => {
     const expectedID = 436036;
     const ne = new Employee("foo",expectedID);
-    expect(ne.getId()).toBe(expectedID);
+    expect(ne.getId()).toEqual(expectedID);
 });
 
 test("If you're able to get email from the method getEmail()", () => {
     const expectedEA = "foo@foo.com";
     const ne = new Employee("foo",0,expectedEA);
-    expect(ne.getEmail()).toBe(expectedEA);
+    expect(ne.getEmail()).toEqual(expectedEA);
 });
 
 test("If getRole() returns the correct employee info", () => {
     const expectedRole = "Employee";
     const ne =new Employee("Loree",0,"foo@foo.com");
-    expect(ne.getRole()).toBe(expectedRole);
+    expect(ne.getRole()).toEqual(expectedRole);
 });
