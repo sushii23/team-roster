c35f20ae45031773db70403aeb4b05052dca833d
Adrian Deon <sushdawg@Adrians-MacBook-Pro.local>
2023-01-19 02:48:09 -0600

    added instance of employee

--- main/__test__/Employee.test.js
+++ main/__test__/Employee.test.js
@@ -1,2 +1,7 @@
 const Employee = require("../lib/Employee");
 
+test("Can an Employee be created", () => {
+const ne = new Employee();
+expect( ne instanceof Employee).toBe(true);
+});
+
--- main/__test__/Manager.test.js
+++ main/__test__/Manager.test.js
@@ -1,3 +1,3 @@
-//Manager test implements Employee test//
+//Manager test implements Employee test,Manager is the parent//
 const Manager = require("../lib/Manager");
 const Employee = require("../lib/Employee");
