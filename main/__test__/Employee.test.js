0e686947c68aa4f961dd4a9dcbb315e1ff092d7b
Adrian Deon <sushdawg@Adrians-MacBook-Pro.local>
2023-01-19 04:52:21 -0600

    added id and email test

--- main/__test__/Employee.test.js
+++ main/__test__/Employee.test.js
@@ -8,6 +8,17 @@ test("If an Employee can be created", () => {
 test("If the name can be created using constructor class", () => {
  const expectedName = "Loree";
  const ne = new Employee(expectedName);
- expect(ne.expectedName).toBe(expectedName);
+ expect(ne.name).toBe(expectedName);
 });
 
+test("If the id can be set through constuctor", () => {
+  const expectedID = 436036;
+  const ne = new Employee("foo",expectedID);
+  expect(ne.id).toBe(expectedID);
+});
+
+test("if the email can be set through constructor", () => {
+  const expectedEA = "foo@foo.com";
+  const ne = new Employee("foo",0,expectedEA);
+  expect(ne.email).toBe(expectedEA);
+});
