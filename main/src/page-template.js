6afdd1ac50d28858a2331ae2ff64229badbaf09c
Adrian Deon <sushdawg@Adrians-MBP.frontiernet.net>
2023-01-25 03:40:09 -0600

    made function to generate html

--- main/src/page-template.js
+++ main/src/page-template.js
@@ -3,7 +3,7 @@ const { constant } = require("lodash");
 const makeTeam =team => {
 
 const developManager = manager => {
-    `
+    return`
 <div class="card em-card">
     <div class="card-header">
         <h2 class="card-title">${manager.name}</h2>
@@ -22,7 +22,7 @@ const developManager = manager => {
 
 
 const developEngineer = engineer => {
-    `
+    return`
     <div class="card em-card">
         <div class="card-header">
             <h2 class="card-title">${engineer.name}</h2>
@@ -40,7 +40,7 @@ const developEngineer = engineer => {
 };
 
 const developIntern = intern => {
-    `
+    return`
     <div class="card em-card">
         <div class="card-header">
             <h2 class="card-title">${engineer.name}</h2>
@@ -75,31 +75,68 @@ for (let i = 0; i < team.length; i++) {
     }
 }
 return html.join("");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 };
+
+module.exports = team => {
+
+    return `
+    <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title>My Team</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/c502137733.js"></script>
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 jumbotron mb-3 team-head">
+                <h1 class="text-center">Team</h1>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="team-section col-12 d-flex justify-content-center">
+                ${makeTeam(team)}
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+    `;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
