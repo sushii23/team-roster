f7adf242ba6b03ed351869651b370ba13a78fde5
Adrian Deon <sushdawg@Adrians-MBP.frontiernet.net>
2023-01-25 05:36:27 -0600

    created index.js

--- main/__test__/Manager.test.js
+++ main/__test__/Manager.test.js
@@ -4,19 +4,19 @@ const Employee = require("../lib/Employee");
 
 describe("Manager", () => {
     it("should set office number through using the constuctor", () => {
-      const expectedOfn =95;
+      const expectedOfn =9;
       const nem = new Manager("foo", 0, "foo@foo.com", expectedOfn);
       expect(nem.officeNumber).toEqual(expectedOfn);
       });
    
     it("getRole() returns the correct Manager info", () => {
         const expectedRole = "Manager";
-        const nem =new Manager("foo",0,"foo@foo.com", 95);
+        const nem =new Manager("foo",0,"foo@foo.com", 9);
         expect(nem.getRole()).toEqual(expectedRole);
     });
    
     it("can get office number through getOffice()", () => {
-      const expectedOfn = 95;
+      const expectedOfn = 9;
       const nem = new Manager("foo",0,"foo@foo.com",expectedOfn)
       expect(nem.getOfficeNumber()).toEqual(expectedOfn);
     });
---
+++ main/index.js
@@ -0,0 +1,161 @@
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const DIST_DIR = path.resolve(__dirname, "dist");
+const distPath = path.join(DIST_DIR, "team.html");
+
+const render = require("./src/page-template.js");
+
+let teamMembers = [];
+let idArray = [];
+
+console.log("Welcome! Use `npm run reset` to reset the dist/ folder\n");
+
+async function createManager() {
+  console.log("Please build your team 👥");
+  const { managerName, managerId, managerEmail, managerOfficeNumber } = await inquirer.prompt([
+    {
+      type: "input",
+      name: "managerName",
+      message: "Enter the team managers name.",
+      validate: (answer) => (answer !== "") || "Please enter at least one character.",
+    },
+    {
+      type: "input",
+      name: "managerId",
+      message: "Enter the team manager's id.",
+      validate: (answer) => (answer.match(/^[0-9]\d*$/) && true) || "Enter a positive number.",
+    },
+    {
+      type: "input",
+      name: "managerEmail",
+      message: "Enter the team manager's email.",
+      validate: (answer) => (answer.match(/\S+@\S+\.\S+/) && true) || "Enter a valid email address.",
+    },
+    {
+      type: "input",
+      name: "managerOfficeNumber",
+      message: "Enter the team manager's office number",
+      validate: (answer) => (answer.match(/^[1-9]\d*$/) && true) || "Enter a positive number greater than zero.",
+    },
+  ]);
+  teamMembers.push(new Manager(managerName, managerId, managerEmail, managerOfficeNumber));
+  idArray.push(managerId);
+  await createTeam();
+}
+
+async function createTeam() {
+  const { memberChoice } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "memberChoice",
+      message: "Pick the type of team member you would like to add.",
+      choices: ["Engineer", "Intern", "I don't want to add any more team members."],
+    },
+  ]);
+  switch (memberChoice) {
+    case "Engineer":
+      await addEngineer();
+      break;
+    case "Intern":
+      await addIntern();
+      break;
+    default:
+      buildTeam();
+  }
+}
+
+async function addEngineer() {
+  const { engineerName, engineerId } = await inquirer.prompt([
+    {
+      type: "input",
+      name: "engineerName",
+      message: "Enter the name of the engineer.",
+      validate: (answer) => (answer !== "") || "Please enter at least one character.",
+    },
+    {
+      type: "input",
+      name: "engineerId",
+      message: "Enter the engineer's id.",
+      validate: async (answer) => {
+        const pass = answer.match(/^[0-9]\d*$/);
+        if (pass) {
+          if (idArray.includes(answer)) {
+            return "Please enter a different number.";
+          } else {
+            return true;
+          }
+        }
+        return " Enter a positive number.";
+      },
+    },
+    {
+      type: "input",
+      name: "engineerEmail",
+      message: "Enter the engineer's email.",
+      validate: (answer) => (answer.match(/\S+@\S+\.\S+/) && true) || "Enter a valid email address.",
+    },
+    {
+      type: "input",
+      name: "engineerGithub",
+      message: "Enter the engineer's GitHub username.",
+      validate: (answer) => (answer !== "") || "Please enter at least one character.",
+    },
+  ]);
+  teamMembers.push(new Engineer(engineerName, engineerId, engineerEmail, engineerGithub));
+  idArray.push(engineerId);
+  await createTeam();
+}
+
+async function addIntern() {
+  const { internName, internId } = await inquirer.prompt([
+    {
+      type: "input",
+      name: "internName",
+      message: "Enter the name of the intern.",
+      validate: (answer) => (answer !== "") || "Please enter at least one character.",
+    },
+    {
+      type: "input",
+      name: "internId",
+      message: "Enter the intern's id.",
+      validate: async (answer) => {
+        const pass = answer.match(/^[0-9]\d*$/);
+        if (pass) {
+          if (idArray.includes(answer)) {
+            return "Please enter a different number.";
+          } else {
+            return true;
+          }
+        }
+        return " Enter a positive number.";
+      },
+    },
+    {
+      type: "input",
+      name: "internEmail",
+      message: "Enter the intern's email.",
+      validate: (answer) => (answer.match(/\S+@\S+\.\S+/) && true) || "Enter a valid email address.",
+    },
+    {
+      type: "input",
+      name: "internSchool",
+      message: "Enter the intern's school.",
+      validate: (answer) => (answer !== "") || "Please enter at least one character.",
+    },
+  ]);
+  teamMembers.push(new Intern(internName, internId, internEmail, internSchool));
+  idArray.push(internId);
+  await createTeam();
+}
+
+async function buildTeam() {
+  fs.writeFileSync(distPath, render(teamMembers), "utf-8");
+}
+
+createManager();
+
